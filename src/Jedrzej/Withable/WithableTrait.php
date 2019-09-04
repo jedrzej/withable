850c2fc1fca92d8b0ddbb75270374967e33cf11d
katsuren <t@k2rn.com>
2019-09-04 14:21:40 +0900

    Update for Laravel 6.0 Remove deprecated helper function and use Facade

--- src/Jedrzej/Withable/WithableTrait.php
+++ src/Jedrzej/Withable/WithableTrait.php
@@ -1,7 +1,6 @@
 <?php namespace Jedrzej\Withable;
 
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Support\Facades\Input;
 use RuntimeException;
 
 trait WithableTrait
@@ -42,7 +41,7 @@ trait WithableTrait
 
     protected function getWithRelationsList($relations = null)
     {
-        return $relations ? (array)$relations : (array)Input::get($this->withParameterName, []);
+        return $relations ? (array)$relations : (array)request()->get($this->withParameterName, []);
     }
 
     /**
